a8e9a62a3f2a7756f6101e1f9f17a8b82cc4f534
sgk98 <shyamgopal@protonmail.com>
2017-11-06 13:34:05 +0530

    BFS added:

--- src/par1.cpp
+++ src/par1.cpp
@@ -1,11 +1,21 @@
 #include<bits/stdc++.h>
+#include<omp.h>
+#include <parallel/algorithm>
 using namespace std;
 typedef long long ll;
 #define N_MAX 1000000
 vector <int> adj[N_MAX];
 int vis[N_MAX];
 int outdeg[N_MAX];
-long double ran[N_MAX];
+int ran_unsort[N_MAX];
+long double b_min;
+vector < pair<long double,int> > ran(N_MAX);
+vector <int> q;
+int N,M;
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return (a.first > b.first);
+}
 long double max(int N)
 {
     long double max_val;
@@ -14,35 +24,105 @@ long double max(int N)
     #pragma omp parallel for reduction(max:max_val)
     for(idx=0;idx<N;idx++)
     {
-        max_val = max_val > ran[idx] ? max_val : ran[idx];
+        max_val = max_val > ran[idx].first ? max_val : ran[idx].first;
     }
     return max_val;
 }
-
+void BFS()
+{
+    omp_set_nested(1);
+    int idx=1;
+    cerr<<ran[0].second;
+    q.push_back(ran[0].second);
+    vis[ran[0].second]=ran[0].second;
+    int prev=0;
+    int time=1;
+    
+    while(1)
+    {
+        int flag=0;
+        int curn=q.size();
+        #pragma omp parallel for
+        for(int i=prev;i<curn;i++)
+        {
+        //    cerr<<i<<endl;
+            int v=q[i];
+            #pragma omp parallel for
+            for(int j=0;j<adj[v].size();j++)
+            {
+                int ver=adj[v][j]; 
+                #pragma omp crtical
+                {
+                    if(vis[ver]==0)
+                    {
+                        vis[ver]=v;
+                        q.push_back(ver);
+                        flag=1;
+                    }
+                }
+            }
+        }
+        
+        ++time;
+        prev=curn;
+        if(prev==N)
+            break;
+        if(q.size()>=N)
+            break;
+        while(1)
+        {
+            if(idx==N+1)
+                break;
+            if(b_min-ran[idx].first<time)
+            {
+                ++idx;
+                if(vis[ran[idx-1].second]==0)
+                    q.push_back(ran[idx-1].second);
+            }
+            else
+                break;
+        }
+    }
+    return ;
+}
 int main(int argc,char* argv[])
 {
-	string bet=argv[1];
-	long double beta = ::atof(bet.c_str());
+    omp_set_num_threads(40);
+    string bet=argv[1];
+    int threads=stoi(argv[2]);
+    omp_set_num_threads(threads);
+    long double beta = ::atof(bet.c_str());
     random_device rd;
     mt19937 gen(rd());
     exponential_distribution<> d(beta);
+    
+    cin>>N>>M;
     int n,m;
-    n=1000000;
-    //cin>>n>>m;
-    int start=clock();
-    #pragma omp parallel for
-    for(int i=0;i<n;i++)
+    n=N;
+    m=M;
+    for(int i=0;i<m;i++)
     {
-    	ran[i]=d(gen);
-
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    /*for(int i=0;i<n;i++)cout<<ran[i]<<endl;
-    cout<<"ans is"<<endl;*/
-    max(n);
-    int stop=clock();
-    cout << "time: " << (stop-start)/double(CLOCKS_PER_SEC)*1000 << endl;
-
-
+    cout<<"IO Done"<<endl;
+    //int start=clock();
+    double time1=omp_get_wtime();
+    #pragma omp parallel for
+    for(int i=0;i<=n;i++)
+    {
+    	ran[i].first=d(gen);
+        ran[i].second=i;
+        ran_unsort[i]=ran[i].first;
+    }
+    
+    b_min=max(n);
+    __gnu_parallel::sort(ran.begin(),ran.end(),sortinrev);
+    BFS();
+    cout<<omp_get_wtime()-time1<<endl;
+    
 
     return 0;
 }
